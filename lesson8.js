f4ee3293c90457c04d1215fdacd628fb4e2b12f2
matthew9510@gmail.com <matthew9510@gmail.com>
2019-07-17 13:00:14 -0700

    Fix: switched logic of filtering people under 50 and also switched for loop for filter and for each statements

--- lesson8.js
+++ lesson8.js
@@ -6,12 +6,20 @@ var people = [
     { name: "Betty", age: 37 }
     ]
 
-var nameFilter = function() {
-    for (var i = 0; i < people.length; i++) {
-        let name = people[i].name
-        if (name > 50) {
-            console.log("My Name is " + name)
-        }
-    }
-}
-nameFilter()
+var isUnder50 = (person) => person.age < 50
+var under50 = people.filter(isUnder50)
+
+var printName = (person) => console.log("My name is", person["name"])
+under50.forEach(printName);
+
+// var nameFilter = function() {
+//     for (var i = 0; i < people.length; i++) {
+//         let person = people[i]
+//         let name = person.name
+//         let age = person.age
+//         if (age < 50) {
+//             console.log("My Name is " + name)
+//         }
+//     }
+// }
+// nameFilter()
